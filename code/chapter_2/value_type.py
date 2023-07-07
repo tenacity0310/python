3b866a7a1cf5b6ada64a3a7aa07d3ad4c799ba00
Peter Li <thpeterli@gmail.com>
2023-07-07 23:20:10 +0800

    add value_type.py

---
+++ code/chapter_2/value_type.py
@@ -0,0 +1,10 @@
+# For head First Chapter 2
+# Numbers
+# the variable wait_time is assigned the integer type. 
+import random 
+wait_time=random.randint(1,60)
+print("The number is:",wait_time)
+# String
+# the variable word is assigned the string type.
+word='bottles'
+print(word)
